1e5570a11a523259225dd70f5af7ebdaaac402db
root <root@172.16.0.1>
2023-01-04 20:01:49 -0500

    primer commit

--- config_example.py
+++
@@ -1,7 +0,0 @@
-BOT_TOKEN = 'QWQ.qwq.QWQ'  # Discord bot token
-COMMANDS_CHANNEL_ID = 1025739706069160026  # #commands channel id
-NOTIFICATIONS_CHANNEL_ID = 1026119089674002444  # #level-notifications channel id
-BOT_ADMIN = [728005293665026189]
-GAME_ADMIN_ROLE = 1025707110706724894
-ENGINE_TRIBE_HOST = 'http://enginetribe.host:port'
-ENGINE_TRIBE_API_KEY = 'enginetribe'
